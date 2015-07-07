1ec75e26fff5c5efadcaa32f62259d86545c45bc
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2015-07-07 09:17:15 +0100

    Lodash for Python

---
+++ sample.py
@@ -0,0 +1,39 @@
+#Lodash for python
+def unique(l):
+	'''
+	This function generate a list of unique values from l
+	'''
+	pass
+
+def reverse(s):
+	'''
+	This function generates the reverse of s. s can be a string or a list. It returns the type given to it
+	'''
+	pass
+
+def intersection(a,b):
+	'''
+	This function returns the intersection of a and b - A list of common elements between a and b
+	'''
+	pass
+
+def generate(steps):
+	pass
+
+def parse_csv(csv_string):
+	'''
+	This function parses a CSV string as a list. The specification demands that the first row of data represents the column names
+	'''
+	pass
+
+def frequency(needle, haystack):
+	'''
+	This function returns the number of times needle appears in haystack
+	'''
+	pass
+
+def sort(l):
+	'''
+	This function returns a sorted version of l
+	'''
+	pass
